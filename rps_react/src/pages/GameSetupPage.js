ee60f49ec0115989098c9317f3ade0a60802a1e9
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-18 21:35:34 -0700

    Update GameSetupPage.js

--- rps_react/src/pages/GameSetupPage.js
+++ rps_react/src/pages/GameSetupPage.js
@@ -1,4 +1,15 @@
+import SiteName from '../components/SiteName'
+import Nav from '../components/Nav'
 const GameSetupPage = () => {
+    return (
+        <div>
+        <div className='flex justify-between items-center'>
+        <SiteName />
+        <Nav />
+        </div>
+        </div>
+
+    )
 
 }
 export default GameSetupPage
