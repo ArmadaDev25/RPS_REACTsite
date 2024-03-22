e72e08c70b4a82933dbb8664058f86ca4f230e05
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-21 19:58:55 -0700

    Made a local JSX element within the game play page to hold the bottons that allow the player to chose between Rock, Paper, or scissors

---
+++ rps_react/src/components/game/PlayVsCpu.js
@@ -0,0 +1,10 @@
+const PlayVScpu = () => {
+
+    return(
+        <div>
+            <h1>Play Vs CPU</h1>
+        </div>
+    )
+
+}
+export default PlayVScpu
--- rps_react/src/pages/PlayGamePage.js
+++ rps_react/src/pages/PlayGamePage.js
@@ -1,17 +1,45 @@
 // Page Where the player plays the actual game
 import SiteName from '../components/SiteName'
 import Nav from '../components/Nav'
+import PlayVScpu from '../components/game/PlayVsCpu'
+
 const PlayGamePage = ( props ) => {
     console.log(props.gamemode)
-    return (
-        <div className='h-screen bg-background'>
-        <div className='flex justify-between items-center'>
-        <SiteName />
-        <Nav />
-        </div>
-        </div>
+    
+    
+    // Local JSX element that holds the game buttons
+    const Gamebuttons = () => {
+        
+        return (
+            <div>
+                <button>Rock</button>
+                <button>Paper</button>
+                <button>Scissors</button>
 
-    )
+            </div>
+        )
+
+    }
+    
+
+    if (props.gamemode == 1) {
+        return (
+            <div className='h-screen bg-background'>
+            <div className='flex justify-between items-center'>
+            <SiteName />
+            <Nav />
+            </div>
+            <PlayVScpu />
+            <Gamebuttons />
+            </div>
+    
+        )
+
+    }
+
+
+    
 
 }
 export default PlayGamePage
+
