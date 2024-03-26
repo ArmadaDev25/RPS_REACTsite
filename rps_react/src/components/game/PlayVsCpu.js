fa80fc989baa6fa9d719769dc968c759130ab4c1
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-25 18:32:28 -0700

    PlayVsCpu now makes a choice between rock paper and scissors after the player inputs there selection using one of the 3 buttons each time the player plays

--- rps_react/src/components/game/PlayVsCpu.js
+++ rps_react/src/components/game/PlayVsCpu.js
@@ -1,47 +1,39 @@
 import { useState, useEffect } from "react"
 
 const PlayVScpu = (props) => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = ['Rock', 'Paper', 'Scissors']
     const [cpuChoice, setCpuChoice] = useState(null)
-    const [refresh, setRefresh] = useState(false)
 
-    function getNumChoice() {
-        
-        
-
-
-    }
     function makeCPUchoice(){
-        console.log("this has run")
+        console.log("This has run")
         setCpuChoice(choices[Math.floor(Math.random()* 3)])
     }
 
-    function resetcpuchoice(){
-        setCpuChoice(null)
-    }
-
-    useEffect(() =>{
-        
-        if(cpuChoice == null){
-            makeCPUchoice()
-        }
-        if(refresh == false){
-            makeCPUchoice()
-            console.log(refresh)
-            console.log("This is what the CPU Choose")
-            console.log(cpuChoice)
-        }
-        
-    },[refresh])
     
-   if (props.playerchoice != null){
-        if (refresh == false){
-            setRefresh()
-        }
-        
+
+  
+
+    //Component that contains the logic for making a choice 
+    const CpuLogicComp = () => {
+        useEffect(() => {
+            makeCPUchoice()
+
+        },[])
+        return(
+            <div>
+                <h1>The CPU Choose: {cpuChoice}</h1>
+            </div>
+        )
+    }
+    
+   if (props.playerchoice != null){    
+        console.log(props)
         return(
             <div>
                 <h1>Play Vs Computer</h1>
+                <CpuLogicComp />
+                <h1>The Player Choose: {props.playerchoice}</h1>
+
             </div>
         )    
    }
