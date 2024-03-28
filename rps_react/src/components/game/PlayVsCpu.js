2d59e026685137be7f42361195b31b63b88edcab
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-28 16:38:50 -0700

    playerchoice and cpuchoice are properly fed into the function

--- rps_react/src/components/game/PlayVsCpu.js
+++ rps_react/src/components/game/PlayVsCpu.js
@@ -16,6 +16,7 @@ const PlayVScpu = (props) => {
     const CpuLogicComp = () => {
         useEffect(() => {
             makeCPUchoice()
+            getWinner(props.playerchoice, cpuChoice)
             
 
         },[])
