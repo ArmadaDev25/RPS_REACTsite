09926dc696745937bdb0d057c94c1f228b592ecc
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-27 17:33:13 -0700

    Added heading and subheading to the about page

--- rps_react/src/pages/AboutPage.js
+++ rps_react/src/pages/AboutPage.js
@@ -10,6 +10,8 @@ const AboutPage = () => {
                 <SiteName />
                 <Nav />
             </div>
+            <h1 className="text-7xl font-bold text-[#00df9a]">The Development of RPS React</h1>
+            <h1>From Python Script to web app.</h1>
 
         </div>
         
