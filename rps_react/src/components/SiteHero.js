0673aa34d254312d0b89c4050ddc408ee89df63d
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-27 17:54:28 -0700

    Moved the hero into its own component and worked on the layout

---
+++ rps_react/src/components/SiteHero.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+const SiteHero = () => {
+    return (
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <h1 className="text-9xl font-bold text-[#00df9a]">
+            RPS REACT
+            </h1>
+            <h2>The online Rock Paper Scissors game made in React</h2>
+            <HeroButtons />
+        </div>
+    )
+
+}
+// Component that simply holds the buttons for only the hero on the homepage
+const HeroButtons = () => {
+    return (
+        <div>
+            <Link to="/playvscpu">
+                <button className='bg-Green hover:bg-Green-dark px-10 py-5 '>
+                        <h1 className='text-white'>Play Now</h1>
+                </button>
+            </Link>
+            <Link to="/about">
+                <button className='bg-Green hover:bg-Green-dark px-10 py-5 '>
+                        <h1 className='text-white'>Learn More</h1>
+                </button>
+            </Link>
+        </div>
+    )
+}
+export default SiteHero
--- rps_react/src/pages/Homepage.js
+++ rps_react/src/pages/Homepage.js
@@ -1,6 +1,6 @@
-
+import SiteHero from '../components/SiteHero'
 import Nav from '../components/Nav'
-import { Link } from 'react-router-dom'
+
 
 const Homepage = () => {
     return(
@@ -8,34 +8,8 @@ const Homepage = () => {
         <div className='flex justify-center'>
             <Nav />
         </div>
-        <h1 className="text-7xl font-bold text-[#00df9a]">
-        RPS REACT
-        </h1>
-        <h2>The online Rock Paper Scissors game made in React</h2>
-        <HomepageButtons />
-        </div>
-    
-    )
-    
-}
-
-
-// Component that simply holds the buttons for only the homepage
-const HomepageButtons = () => {
-    return (
-        <div>
-            <Link to="/playvscpu">
-                <button className='bg-Green hover:bg-Green-dark px-10 py-5 '>
-                        <h1 className='text-white'>Play Now</h1>
-                </button>
-            </Link>
-            <Link to="/about">
-                <button className='bg-Green hover:bg-Green-dark px-10 py-5 '>
-                        <h1 className='text-white'>Learn More</h1>
-                </button>
-            </Link>
+        <SiteHero />
         </div>
     )
 }
-
 export default Homepage
